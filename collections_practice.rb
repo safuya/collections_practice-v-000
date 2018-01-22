5bd76c21ffa11408c37af7a0d014c46daf277fb1
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-22 01:26:50 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  a = array[1]
+  b = array[2]
+  array[1] = b
+  array[2] = a
+  array
+end
+
+def reverse_array(array)
+  array.sort { |a, b| 1 }
+end
+
+def kesha_maker(array)
+  array.each { |word| word[2] = "$" }
+end
+
+def find_a(array)
+  array.delete_if { |word| word[0] != "a" }
+end
+
+def sum_array(array)
+  sum = 0
+  array.each { |n| sum += n }
+  sum
+end
+
+def add_s(array)
+  a = array[1]
+  array.map! { |word| "#{word}s" }
+  array[1] = a
+  array
+end
