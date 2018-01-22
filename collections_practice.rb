fa479cc437681b9128239e6e7d3d5e23f604ce08
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-22 00:59:54 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,7 +12,7 @@ end
 
 def swap_elements(array)
   new_array = array
-  new_array[1] = array.slice[1..2].sort { |a, b| 1 }[0]
-  new_array[2] = array.slice[1..2].sort { |a, b| 1 }[1]
+  new_array[1] = array.slice(1..2).sort { |a, b| 1 }[0]
+  new_array[2] = array.slice(1..2).sort { |a, b| 1 }[1]
   new_array
 end
